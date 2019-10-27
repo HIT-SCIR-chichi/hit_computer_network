8370a1e8c1aadd7d56295e017dfd3049f47e26a3
1172510217 <2584363094@qq.com>
2019-10-27 20:26:44 +0800

    完善结构+加上注释

--- lab1/src/lab1/CommunicateThread.java
+++ lab1/src/lab1/CommunicateThread.java
@@ -12,8 +12,12 @@ import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
+import java.util.TimeZone;
 
 public class CommunicateThread extends Thread {
 
@@ -22,12 +26,16 @@ public class CommunicateThread extends Thread {
   String request_gram = "";// 接收来自客户端的请求报文
   String respose_gram = "";// 接受来自服务器的响应报文
   int socket_time_out = 1000;// socket的超时时间，设置为1000ms
-  byte[] respose_byte;
-  int port = 80;
-  String method;
-  String URL;
-  String host;
+  byte[] respose_byte;// 用于储存接受的响应报文字节流信息
+  int port = 80;// 默认与服务器的连接端口为80
+  String URL;// 头部行中的URL
+  String host;// 头部行中的host
 
+  /**
+   * to:设置client_socket与客户端建立连接通信.
+   * 
+   * @param client_socket 与客户端通信的socket
+   */
   public CommunicateThread(Socket client_socket) {
     this.client_socket = client_socket;
   }
@@ -40,9 +48,10 @@ public class CommunicateThread extends Thread {
       return true;
     }
     BufferedReader bfr_filter =
-        new BufferedReader(new FileReader(HTTP_Proxy.filter_file));
+        new BufferedReader(new FileReader(HTTP_Proxy.configuraion_file));
     String line_filter = "";
     while ((line_filter = bfr_filter.readLine()) != null) {
+      // 若开启了用户过滤且客户端用户为被限制用户，则返回false
       if (HTTP_Proxy.user_filter
           && line_filter.contains(client_socket.getInetAddress().getHostAddress())
           && line_filter.contains("user_filter")) {
@@ -51,17 +60,17 @@ public class CommunicateThread extends Thread {
         bfr_filter.close();
         return false;
       } else if (HTTP_Proxy.web_filter && line_filter.contains(this.host)
-          && line_filter.contains("web_filter")) {
+          && line_filter.contains("web_filter")) {// 若开启了网站过滤且目的主机为被过滤的主机，则返回
         System.out.println("你不能访问该网站，因为目标网站被过滤:" + this.host);
         bfr_filter.close();
         return false;
       } else if (HTTP_Proxy.phishing && line_filter.contains(this.host + " ")
-          && line_filter.contains("phishing")) {
+          && line_filter.contains("phishing")) {// 若开启了网站引导且目的主机为被引导的主机则将头部行替换
         this.host = line_filter.split(" ")[1];
         String old_URL = this.URL;
         this.URL = "http://" + this.host + "/";
         this.port = 80;
-        request_gram = request_gram.replace(old_URL, this.URL);
+        request_gram = request_gram.replace(old_URL, this.URL);// 将请求报文中的头部URL替换为引导网站的URL
         System.out.println("你不能访问目标网站，因为该网站已被引导向:" + this.host);
         bfr_filter.close();
         return true;
@@ -71,67 +80,51 @@ public class CommunicateThread extends Thread {
     return true;
   }
 
+  /**
+   * to:判断是否有缓存文件.
+   */
   public void in_cache_new() throws IOException {
     if (!new File("src/file/" + this.host).exists()) {
       new File("src/file/" + this.host).mkdir();
     }
     File cache_file =
-        new File("src/file/" + this.host + "/" + this.URL.hashCode() + ".dat");
-    if (!cache_file.exists()) {
-      cache_file.createNewFile();
-      // 向服务器转发原请求
-      PrintWriter proxy_out = new PrintWriter(server_socket.getOutputStream());
-      proxy_out.write(request_gram);
+        new File("src/file/" + this.host + "/" + this.URL.hashCode() + ".txt");
+    PrintWriter proxy_out = new PrintWriter(server_socket.getOutputStream());// 向服务器发送的流
+    InputStream proxy_server_in = server_socket.getInputStream();// 服务器向客户端返回响应的流
+    OutputStream proxy_client_out = client_socket.getOutputStream();// 向客户端发送的流
+    if (!cache_file.exists()) {// 若对应的缓存文件不存在，则创建该文件用于记录新返回的响应报文
+      FileOutputStream cache_file_out = new FileOutputStream(cache_file);// 写缓存文件的流
+      proxy_out.write(request_gram); // 向服务器转发原请求
       proxy_out.flush();
-      InputStream proxy_server_in = server_socket.getInputStream();
-      OutputStream proxy_client_out = client_socket.getOutputStream();
-      List<Byte> out_bytes = new ArrayList<>();
       while (true) {
         try {
-          server_socket.setSoTimeout(this.socket_time_out);
-          int b = proxy_server_in.read();
+          server_socket.setSoTimeout(this.socket_time_out);// 设置超时时间用于跳出阻塞状态
+          int b = proxy_server_in.read();// 字节流读取响应报文
           if (b == -1) {
             break;
           } else {
-            out_bytes.add((byte) (b));
-            proxy_client_out.write(b);
+            cache_file_out.write(b);// 写入到缓存文件中
+            proxy_client_out.write(b);// 写入到向客户端发送响应的流
             server_socket.setSoTimeout(0);
           }
         } catch (SocketTimeoutException e) {
           break;
         }
       }
-      this.respose_byte = new byte[out_bytes.size()];
-      int count = 0;
-      for (Byte byte1 : out_bytes) {
-        this.respose_byte[count++] = byte1;
-      }
-      FileOutputStream cache_file_out = new FileOutputStream(cache_file);
-      cache_file_out.write(this.respose_byte);
       cache_file_out.close();
     } else {// 文件已经存在，则需要判断：若存在DATA且已更新，则返回，否则直接将缓存作为响应
-      BufferedReader cache_reader =
-          new BufferedReader(new InputStreamReader(new FileInputStream(cache_file)));
-      String line_cache = "";
-      while ((line_cache = cache_reader.readLine()) != null) {
-        this.respose_gram += line_cache + "\r\n";
-        if (line_cache.startsWith("Date:")) {
-          this.request_gram = this.request_gram.replace("\r\n\r\n",
-              "\r\n" + "If-Modified-Since: " + line_cache.substring(6) + "\r\n\r\n");
-        }
-      }
-      // 向服务器发送新的修改请求报文
-      PrintWriter proxy_out = new PrintWriter(server_socket.getOutputStream());
-      proxy_out.write(request_gram);
+      DateFormat df = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss z", Locale.ENGLISH);
+      df.setTimeZone(TimeZone.getTimeZone("GMT"));
+      this.request_gram = this.request_gram.replace("\r\n\r\n",
+          "\r\nIf-Modified-Since: " + df.format(cache_file.lastModified()) + "\r\n\r\n");
+      proxy_out.write(request_gram);// 发送构造的新的请求报文，添加了文件的最后修改时间
       proxy_out.flush();
-      cache_reader.close();
       // 接收服务器的请求
-      InputStream proxy_server_in = server_socket.getInputStream();
       List<Byte> out_bytes = new ArrayList<>();
       while (true) {
         try {
-          server_socket.setSoTimeout(this.socket_time_out);
-          int b = proxy_server_in.read();
+          server_socket.setSoTimeout(this.socket_time_out);// 设置超时时间用于跳出流阻塞
+          int b = proxy_server_in.read();// 读取响应报文
           if (b == -1) {
             break;
           } else {
@@ -143,42 +136,45 @@ public class CommunicateThread extends Thread {
         }
       }
       this.respose_byte = new byte[out_bytes.size()];
-      int count = 0;
+      int count = 0;// 用于构造响应字节报文
       for (Byte byte1 : out_bytes) {
         this.respose_byte[count++] = byte1;
       }
-      this.respose_gram = new String(respose_byte, 0, count);
-      if (this.respose_gram.split("\r\n")[0].contains("304")) {
-        // System.out.println("服务器报文未更新，可以直接请求缓存\n该条请求报文在缓存中\n" + this.request_gram);
-        System.out.println("缓存命中数: " + ++HTTP_Proxy.cache_hit + "\t命中: " + this.URL);
-        // 直接将缓存报文发送给客户端
+      this.respose_gram = new String(respose_byte, 0, count);// 构造文本响应报文
+      if (this.respose_gram.split("\r\n")[0].contains("304")) {// 响应报文头含304，则缓存可用
+        System.out.println("缓存命中数: " + (++HTTP_Proxy.cache_hit) + "\t命中: " + this.URL
+            + "\t文件名: " + this.URL.hashCode());
         FileInputStream cache_file_read = new FileInputStream(cache_file);
-        OutputStream proxy_client_out = client_socket.getOutputStream();
-        int b;
+        int b;// 直接将缓存报文发送给客户端
         while ((b = cache_file_read.read()) != -1) {
-          proxy_client_out.write(b);
+          proxy_client_out.write(b);// 写入客户端的流
         }
         cache_file_read.close();
-      } else if (this.respose_gram.split("\r\n")[0].contains("200")) {
-        // 将从服务器读取到的转发给客户端，并更新缓存
-        OutputStream proxy_client_out = client_socket.getOutputStream();
-        proxy_client_out.write(this.respose_byte);
+      } else if (this.respose_gram.split("\r\n")[0].contains("200")) {// 响应报文头含200，更新缓存
+        FileOutputStream cache_file_out = new FileOutputStream(cache_file);// 写缓存文件的流
+        proxy_client_out.write(this.respose_byte);// 将从服务器读取到的转发给客户端
+        cache_file_out.write(this.respose_byte);// 更新本地缓存
+        cache_file_out.close();
       }
     }
   }
 
+  /**
+   * to:多线程的一个线程的run函数，在这个函数里实现代理服务器的所有功能:转发、过滤、引导、缓存.
+   */
   @Override public void run() {
     try {
       BufferedReader bfReader =
-          new BufferedReader(new InputStreamReader(client_socket.getInputStream()));
+          new BufferedReader(new InputStreamReader(client_socket.getInputStream()));// 用于读取客户端发出的请求报文
       String proxy_line = bfReader.readLine();
-      this.parse_request(proxy_line);
+      if (proxy_line == null) {
+        return;
+      }
+      this.parse_request(proxy_line);// 解析头部行，并设置对象的属性值
       while (proxy_line != null) {
         try {
-          if (!proxy_line.contains("Cache-Control")) {
-            request_gram += proxy_line + "\r\n";
-          }
-          client_socket.setSoTimeout(500);
+          request_gram += proxy_line + "\r\n";// 获取请求报文的信息
+          client_socket.setSoTimeout(500);// 设置超时时间，用于跳出流的阻塞状态
           proxy_line = bfReader.readLine();
           client_socket.setSoTimeout(0);
         } catch (SocketTimeoutException e) {
@@ -188,12 +184,12 @@ public class CommunicateThread extends Thread {
       if (!this.filter_and_phishing()) {// 网站过滤，用户过滤，钓鱼
         return;
       }
-      server_socket = new Socket(this.host, this.port);
-      this.in_cache_new();
+      server_socket = new Socket(this.host, this.port);// 建立与服务器通信的套接字
+      this.in_cache_new();// 判断请求报文是否可以由缓存文件给出，并进行相应的操作
       server_socket.close();
       client_socket.close();
     } catch (IOException e) {
-      System.out.println("\n" + e.getMessage() + "\n");
+      System.out.println("\n" + e.getMessage());
     }
   }
 
@@ -201,17 +197,19 @@ public class CommunicateThread extends Thread {
    * in order to:获取目的主机URL以及请求类型；初始化host和port(如果可以)
    */
   public void parse_request(String head_line) {
-    this.method = head_line.split("[ ]")[0];
-    this.URL = head_line.split("[ ]")[1];
+    this.URL = head_line.split("[ ]")[1];// 获取请求的目的URL
     int index = -1;
-    this.host = this.URL;
-    if ((index = this.host.indexOf("http://")) != -1) {
+    this.host = this.URL;// 下面用于获取请求的主机名
+    if ((index = this.host.indexOf("http://")) != -1) {// 去掉URL中的http://
       this.host = this.host.substring(index + 7);
     }
-    if ((index = this.host.indexOf("/")) != -1) {
+    if ((index = this.host.indexOf("https://")) != -1) {// 去掉URL中的https://
+      this.host = this.host.substring(index + 8);
+    }
+    if ((index = this.host.indexOf("/")) != -1) {// 去掉URL中的/
       this.host = this.host.substring(0, index);
     }
-    if ((index = this.host.indexOf(":")) != -1) {
+    if ((index = this.host.indexOf(":")) != -1) {// 去掉URL中的:
       this.port = Integer.valueOf(this.host.substring(index + 1));
       this.host = this.host.substring(0, index);
     }
--- lab1/src/lab1/HTTP_Proxy.java
+++ lab1/src/lab1/HTTP_Proxy.java
@@ -1,40 +1,76 @@
 package lab1;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.net.ServerSocket;
 
 public class HTTP_Proxy {
 
-  static File filter_file = new File("src/file/filter_file.txt");
-  static boolean user_filter = true;
-  static boolean web_filter = true;
-  static boolean phishing = true;
-  static int cache_hit = 0;
-  ServerSocket serverSocket;
-  int server_port = 10240;
+  // 默认的完成过滤网页、过滤用户和网站引导的配置文件
+  static File configuraion_file = new File("src/file/configuration_file.txt");
+  static boolean user_filter = true;// 是否开启用户过滤
+  static boolean web_filter = true;// 是否开启网页过滤
+  static boolean phishing = true;// 是否开启网站引导
+  static ServerSocket server_socket;// 用于监听客户端请求的套接字
+  static int server_port = 10240;// 默认的监听端口号
+  static int cache_hit = 0;// 用于记录缓存命中的次数
 
-  public HTTP_Proxy() throws IOException {
-    this.serverSocket = new ServerSocket(server_port);
-  }
-
-  public static void main(String[] args) throws IOException {
+  /**
+   * to:启动代理服务器，开始处理用户端的HTTP请求.
+   */
+  public static void start_proxy() {
     try {
-      HTTP_Proxy http_Proxy = new HTTP_Proxy();
-      System.out.println("代理服务器:\t运行\n监听端口:\t" + http_Proxy.server_port);
+      server_socket = new ServerSocket(server_port);// 新建一个与客户端通信的套接字
+      System.out.println("代理服务器:\t运行\n监听端口:\t" + server_port);
       if (user_filter) {
-        System.out.println("用户过滤:\t打开");
+        System.out.println("用户过滤:\t开启");
       }
       if (web_filter) {
-        System.out.println("网页过滤:\t打开");
+        System.out.println("网页过滤:\t开启");
       }
-      System.out.println("网页缓存:\t打开");
-      System.out.println("**********************************************************");
-      while (true) {
-        new CommunicateThread(http_Proxy.serverSocket.accept()).start();
+      System.out.println("服务器缓存:\t开启");
+      System.out.println("***************************************************");
+      while (true) {// 不断监听来自客户端的请求
+        new CommunicateThread(server_socket.accept()).start();// 新建子线程处理连接请求
       }
     } catch (IOException e) {
       System.out.println(e.getMessage());
     }
+  }
+
+  /**
+   * to:读取配置文件，用于配置网页过滤、网站引导、用户过滤开否开启，默认缓存开启.
+   * 
+   * @param file_path 配置文件路径名
+   */
+  public static void configurate_proxy(String file_path) {
+    try {
+      if (file_path != null) {
+        HTTP_Proxy.configuraion_file = new File(file_path);
+      }
+      BufferedReader configure_reader =
+          new BufferedReader(new FileReader(HTTP_Proxy.configuraion_file));
+      String configure_line = "";
+      while ((configure_line = configure_reader.readLine()) != null) {
+        if (configure_line.contains("web_filter=")) {
+          HTTP_Proxy.web_filter = Boolean.valueOf(configure_line.substring(11));
+        } else if (configure_line.contains("user_filter=")) {
+          HTTP_Proxy.user_filter = Boolean.valueOf(configure_line.substring(12));
+        } else if (configure_line.contains("phishing=")) {
+          HTTP_Proxy.phishing = Boolean.valueOf(configure_line.substring(9));
+        }
+      }
+      configure_reader.close();
+    } catch (IOException e) {
+      System.out.println(e.getMessage());
+    }
+
+  }
+
+  public static void main(String[] args) {
+    HTTP_Proxy.configurate_proxy(null);// 配置附加功能，参数为null，表示配置文件为默认
+    HTTP_Proxy.start_proxy();// 启动代理服务器
   }
 }
